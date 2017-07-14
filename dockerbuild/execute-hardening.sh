72e175f7448fd128185b888c255de58a1ded28a3
Neiloy <neiloy@redhat.com>
2017-07-14 09:11:45 -0400

    not specifying configs, paring things down to progressively simpler

--- dockerbuild/execute-hardening.sh
+++ dockerbuild/execute-hardening.sh
@@ -3,19 +3,11 @@
 JBOSS_HOME=/opt/eap
 JBOSS_CLI=$JBOSS_HOME/bin/jboss-cli.sh
 JBOSS_MODE=${1:-"standalone"}
-JBOSS_CONFIG=${2:-"$JBOSS_MODE.xml"}
-
-function wait_for_server() {
-    until `$JBOSS_CLI -c`; do
-        sleep 1
-    done
-}
 
 echo "=> Starting WildFly server"
-$JBOSS_HOME/bin/$JBOSS_MODE.sh -c $JBOSS_CONFIG
+$JBOSS_HOME/bin/$JBOSS_MODE.sh
 
 echo "=> Waiting for the server to boot"
-#wait_for_server
 sleep 60
 
 echo "=> Executing the commands"
